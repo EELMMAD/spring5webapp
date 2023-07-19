15641aea7846ff734d8bccfa6398f3b65323c305
EELMMAD <elmiramadadi@gmail.com>
2023-07-19 13:54:00 +0200

    Multi-Currency Money

---
+++ src/main/java/guru/springframework/spring5webapp/Dollar.java
@@ -0,0 +1,12 @@
+package guru.springframework.spring5webapp;
+public class Dollar {
+    int amount;
+
+    public Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    public void times(int multiplier) {
+        amount *= multiplier;
+    }
+}
---
+++ src/test/java/guru/springframework/spring5webapp/MoneyTest.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring5webapp;
+
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MoneyTest {
+    @Test
+    public void testMultiplication() {
+        Dollar five = new Dollar(5);
+        five.times(2);
+        assertEquals(10, five.amount);
+    }
+}
